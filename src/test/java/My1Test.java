374795161d8f5db9c8c0fa85022a7ff567126b91
jiansheng-eb <xujiansheng@easternbay.cn>
2021-07-20 08:57:59 +0800

    master

---
+++ src/test/java/My1Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class My1Test {
+
+  @Test
+  public void t1(){
+    System.out.println("1");
+    int i = 1/0;
+  }
+}
---
+++ src/test/java/My2Test.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class My2Test {
+
+  @Test
+  public void t1(){
+    System.out.println("2");
+  }
+}
