9d756b7d23642f6835c481516d4680eaccaaa2e2
jiansheng-eb <xujiansheng@easternbay.cn>
2021-07-21 08:44:46 +0800

    master

--- src/test/java/My2Test.java
+++ src/test/java/My2Test.java
@@ -5,5 +5,14 @@ public class My2Test {
   @Test
   public void t1(){
     System.out.println("2");
+    String s  = null;
+    final int length = s.length();
+  }
+
+  @Test
+  public void ttt3(){
+    System.out.println("2");
+    String s  = null;
+    int i = 1/0;
   }
 }
